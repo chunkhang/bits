5ef05ff900c9f156d5e83ed568add55227d32e78
Marcus Mu <chunkhang@gmail.com>
2020-02-16 01:46:15 +0800

    Use custom eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,42 @@
 module.exports = {
   root: true,
-  extends: '@react-native-community',
-};
+  env: {
+    es6: true,
+    node: true,
+  },
+  extends: [
+    'airbnb',
+  ],
+  plugins: [
+    'import',
+    'react',
+  ],
+  rules: {
+    indent: ['error', 2],
+    quotes: ['error', 'single'],
+    semi: ['error', 'never'],
+    camelcase: 0,
+    'max-len': 0,
+    'no-plusplus': ['error', {
+      allowForLoopAfterthoughts: true,
+    }],
+    'object-curly-newline': 0,
+    'arrow-body-style': 0,
+    'no-multiple-empty-lines': ['error', {
+      max: 1,
+      maxEOF: 0,
+      maxBOF: 0,
+    }],
+    'import/no-unresolved': [2, {
+      commonjs: true,
+    }],
+    'react/no-array-index-key': 1,
+    'react/jsx-filename-extension': [1, {
+      extensions: ['.js', '.jsx'],
+    }],
+    'react/jsx-one-expression-per-line': 0,
+    'import/order': ['error', {
+      'newlines-between': 'always-and-inside-groups',
+    }],
+  },
+}
--- scripts/checkNode.js
+++ scripts/checkNode.js
@@ -4,6 +4,7 @@ const packageJson = require('../package.json')
 
 const expectedVersion = packageJson.engines.node
 const currentVersion = process.versions.node
+
 if (currentVersion !== expectedVersion) {
   console.log(`Expected node version to be ${expectedVersion}.`)
   console.log('Try running `nvm use`.')
