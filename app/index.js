5ef39d0e0f53e1103e1766fba9a51f0b7345d07c
Marcus Mu <chunkhang@gmail.com>
2020-02-16 23:14:25 +0800

    Move app index to its own directory

---
+++ app/index.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import {
+  SafeAreaView,
+  Text,
+} from 'react-native'
+
+const App = () => {
+  return (
+    <SafeAreaView>
+      <Text>Hello World</Text>
+    </SafeAreaView>
+  )
+}
+
+export default App
