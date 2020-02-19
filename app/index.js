749b05b50613a77d6a26ff50e62bd3920206142b
Marcus Mu <chunkhang@gmail.com>
2020-02-20 01:01:34 +0800

    Remove RootScreen
    Move it to app index

--- app/index.js
+++ app/index.js
@@ -1,11 +1,14 @@
 import React, { useState, useEffect } from 'react'
+import { SafeAreaView } from 'react-native'
 import { NavigationContainer } from '@react-navigation/native'
 import { ThemeProvider } from 'react-native-elements'
 
 import { StoreContext } from '~/contexts'
 import RootStore, { rehydrate } from '~/stores/RootStore'
-import RootScreen from '~/screens/RootScreen'
 import LoadingScreen from '~/screens/LoadingScreen'
+import RootRouter from '~/routers/RootRouter'
+
+import styles from './styles'
 
 const store = new RootStore()
 
@@ -21,7 +24,9 @@ const App = () => {
     <NavigationContainer>
       <StoreContext.Provider value={store}>
         <ThemeProvider>
-          {loading ? <LoadingScreen /> : <RootScreen />}
+          <SafeAreaView style={styles.mainContainer}>
+            {loading ? <LoadingScreen /> : <RootRouter />}
+          </SafeAreaView>
         </ThemeProvider>
       </StoreContext.Provider>
     </NavigationContainer>
