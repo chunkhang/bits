837a09fe9265c10247710ce3e58a782a64b4a20a
Marcus Mu <chunkhang@gmail.com>
2020-02-19 22:46:13 +0800

    Start using alias import

--- app/index.js
+++ app/index.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { NavigationContainer } from '@react-navigation/native'
 import { ThemeProvider } from 'react-native-elements'
 
-import RootScreen from './screens/RootScreen'
+import RootScreen from '~/screens/RootScreen'
 
 const App = () => {
   return (
