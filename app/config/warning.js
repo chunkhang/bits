c77ca478996dbf3b0cdd045db6f55d7e348d0e01
Marcus Mu <chunkhang@gmail.com>
2020-10-04 16:38:03 +0800

    Update warning config

--- app/config/warning.js
+++ app/config/warning.js
@@ -2,8 +2,16 @@ import { LogBox } from 'react-native'
 
 if (__DEV__) {
   LogBox.ignoreLogs([
+    // https://github.com/gitim/react-native-sortable-list/issues/167
     'componentWillReceiveProps',
     'componentWillMount',
+
+    // https://github.com/gitim/react-native-sortable-list/issues/193
     'SortableList',
+
+    // https://github.com/react-navigation/react-navigation/issues/8457
+    'currentlyFocusedField is deprecated',
+
+    'A DynamicStyleSheet was used without any DynamicValues',
   ])
 }
